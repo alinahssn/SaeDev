70f47c36d7814f2bf30eb6a8020945515e17c530
Alina Ul-Hassan <alina.ulhassan47@gmail.com>
2026-05-19 14:27:52 +0200

    Création d'un perso lambda (sans gameLoop)

--- src/main/java/universite_paris8/iut/aulhassan/maphopital/Controleur.java
+++ src/main/java/universite_paris8/iut/aulhassan/maphopital/Controleur.java
@@ -1,8 +1,15 @@
 package universite_paris8.iut.aulhassan.maphopital;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+import universite_paris8.iut.aulhassan.maphopital.modele.Terrain;
 import universite_paris8.iut.aulhassan.maphopital.modele.Terrain;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -15,12 +22,25 @@ public class Controleur implements Initializable {
     @FXML
     private TilePane tilehopital;
 
+    @FXML
+    private Pane panneauJeu; //superposition avec le tilepane sinon decallage des images
+
     private Terrain terrain;
+    private Ennemi ennemi1;
 
     @Override
     public void initialize (URL url, ResourceBundle rb) {
         this.terrain = new Terrain();
+        this.ennemi1 = new Ennemi();
         creerVueModele();
+
+        Circle rondRouge = new Circle(10, Color.RED);
+
+        rondRouge.translateXProperty().bind(ennemi1.xProperty());
+        rondRouge.translateYProperty().bind(ennemi1.yProperty());
+
+        panneauJeu.getChildren().add(rondRouge); //PAS DANS LA TILEMAP
+
     }
 
 
@@ -34,6 +54,7 @@ public class Controleur implements Initializable {
         Image im1 = chargerImage("carré-blanc-cadre-gris-seul.png");
 
         //tilehopital.getChildren().clear();
+        //TailSet
 
         for (int i = 0; i < terrain.getHauteur(); i++) {
             for (int j = 0; j < terrain.getLargeur(); j++) {
---
+++ src/main/java/universite_paris8/iut/aulhassan/maphopital/Ennemi.java
@@ -0,0 +1,33 @@
+package universite_paris8.iut.aulhassan.maphopital;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+import java.util.Random;
+
+public class Ennemi {
+
+    private DoubleProperty x = new SimpleDoubleProperty(50);
+    private DoubleProperty y = new SimpleDoubleProperty(50);
+
+    public void bouger() {
+        int choix = (int) (Math.random() * 4);// 0, 1, 2 ou 3
+
+        if (choix == 0) this.setX(this.getX() + 32);
+        if (choix == 1) this.setX(this.getX() - 32);
+        if (choix == 2) this.setY(this.getY() + 32);
+        if (choix == 3) this.setY(this.getY() - 32);
+    }
+
+    public double getX() { return this.x.get(); }
+    public void setX(double nouveauX) { this.x.set(nouveauX); }
+    public DoubleProperty xProperty() { return this.x; }
+
+    public double getY() { return this.y.get(); }
+    public void setY(double nouveauY) { this.y.set(nouveauY); }
+    public DoubleProperty yProperty() { return this.y; }
+}
+
+
