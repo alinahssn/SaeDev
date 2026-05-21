735b1af8326c800eae05f15caa6ebbc814ada86b
Baptiste Prouteau <bprouteau@iut.univ-paris8.fr>
2026-05-21 11:33:02 +0200

    Ajout premier ennemi + debut de tour

---
+++ src/main/java/universite_paris8/iut/aulhassan/maphopital/modele/Gastrique.java
@@ -0,0 +1,22 @@
+package universite_paris8.iut.aulhassan.maphopital.modele;
+/*
+Description :
+    Version plus faible de l'enrhumé, sans mouchoirs. PV réduits pour justifier la récompense moindre.
+
+ */
+
+public class Gastrique extends Ennemi {
+    private static final int PV_DEPART = 80;
+    private static final int ATTAQUE = 5;
+    private static final int  VITESSE = 1;
+    private static final int RECOMPENSE = 15;
+
+    public Gastrique() {
+        super(PV_DEPART,ATTAQUE,VITESSE,RECOMPENSE);
+    }
+
+    public String toString(){
+        return "Gastrique : PV = "+getPv()+" | ATTAQUE = " + getAttaque()+" | VITESSE = " + getVitesse()+" | RECOMPENSE = " + getRecompense()+" | VIVANT = " + estVivant();
+    }
+
+}
