3cb62659c43b78711ce8b3269e982ed4cfc0d3b6
Baptiste Prouteau <bprouteau@iut.univ-paris8.fr>
2026-06-11 13:58:05 +0200

    Creation Rabique

---
+++ src/main/java/universite_paris8/iut/aulhassan/maphopital/modele/Rabique.java
@@ -0,0 +1,18 @@
+package universite_paris8.iut.aulhassan.maphopital.modele;
+
+public class Rabique extends Ennemi{
+    private static final int PV_DEPART = 100;
+    private static final int ATTAQUE = 10;
+    private static final int  VITESSE = 2;
+    private static final int RECOMPENSE = 30;
+
+    public Rabique() {
+
+        super(PV_DEPART, ATTAQUE, VITESSE, RECOMPENSE);
+    }
+
+    @Override
+    public String toString(){
+        return "Rabique : PV=" + getPv() + "/" + getPvMax() + " | VIVANT=" + estVivant();
+    }
+}
