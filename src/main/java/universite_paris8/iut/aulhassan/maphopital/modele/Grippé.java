ac0d1998d5ed857030f991dd99b2cc06060a276e
Baptiste Prouteau <bprouteau@iut.univ-paris8.fr>
2026-06-11 13:58:31 +0200

    Creation Grippé

---
+++ src/main/java/universite_paris8/iut/aulhassan/maphopital/modele/Grippé.java
@@ -0,0 +1,21 @@
+package universite_paris8.iut.aulhassan.maphopital.modele;
+
+public class Grippé extends Ennemi{
+
+    private static final int PV_DEPART = 100;
+    private static final int ATTAQUE = 10;
+    private static final int  VITESSE = 2;
+    private static final int RECOMPENSE = 30;
+
+    public Grippé() {
+
+        super(PV_DEPART, ATTAQUE, VITESSE, RECOMPENSE);
+    }
+
+    @Override
+    public String toString(){
+        return "Grippé : PV=" + getPv() + "/" + getPvMax() + " | VIVANT=" + estVivant();
+    }
+}
+
+
