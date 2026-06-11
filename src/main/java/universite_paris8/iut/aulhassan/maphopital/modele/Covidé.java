0362f71458511aac61aca3caf4ae28ba0ea7a83f
Baptiste Prouteau <bprouteau@iut.univ-paris8.fr>
2026-06-11 14:00:23 +0200

    Modification Covidé

--- src/main/java/universite_paris8/iut/aulhassan/maphopital/modele/Covidé.java
+++ src/main/java/universite_paris8/iut/aulhassan/maphopital/modele/Covidé.java
@@ -1,8 +1,19 @@
 package universite_paris8.iut.aulhassan.maphopital.modele;
 
-public class Covidé {
+public class Covidé extends Ennemi{
 
-    //essaie de commit etc
+    private static final int PV_DEPART = 100;
+    private static final int ATTAQUE = 10;
+    private static final int  VITESSE = 2;
+    private static final int RECOMPENSE = 30;
 
-    private int id;
+    public Covidé() {
+
+        super(PV_DEPART, ATTAQUE, VITESSE, RECOMPENSE);
+    }
+
+    @Override
+    public String toString(){
+        return "Covidé : PV=" + getPv() + "/" + getPvMax() + " | VIVANT=" + estVivant();
+    }
 }
