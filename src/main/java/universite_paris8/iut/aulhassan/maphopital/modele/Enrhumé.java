868ac44686d6439d0e2ea406f5ae27716fe1dc6e
Baptiste Prouteau <bprouteau@iut.univ-paris8.fr>
2026-06-11 13:58:49 +0200

    Creation Enrhumé

---
+++ src/main/java/universite_paris8/iut/aulhassan/maphopital/modele/Enrhumé.java
@@ -0,0 +1,18 @@
+package universite_paris8.iut.aulhassan.maphopital.modele;
+
+public class Enrhumé extends Ennemi{
+    private static final int PV_DEPART = 100;
+    private static final int ATTAQUE = 10;
+    private static final int  VITESSE = 2;
+    private static final int RECOMPENSE = 30;
+
+    public Enrhumé() {
+
+        super(PV_DEPART, ATTAQUE, VITESSE, RECOMPENSE);
+    }
+
+    @Override
+    public String toString(){
+        return "Enrhumé : PV=" + getPv() + "/" + getPvMax() + " | VIVANT=" + estVivant();
+    }
+}
