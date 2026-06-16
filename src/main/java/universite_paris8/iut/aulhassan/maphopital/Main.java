a3d3d53a205a911db3af699dcd1a0dfa8bc7dcf8
alina <alina.ulhassan47@gmail.com>
2026-06-17 01:30:56 +0200

    Ouverture chambre map

--- src/main/java/universite_paris8/iut/aulhassan/maphopital/Main.java
+++ src/main/java/universite_paris8/iut/aulhassan/maphopital/Main.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 
 public class Main extends Application {
 
-
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("menu.fxml"));
